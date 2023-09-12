dff8db71c34fbab7385d66267273b5e942ff9839
TomasBkk <pr0digy1@hotmail.com>
2023-09-12 10:50:28 +0200

    updated

--- TestApp/get_last_tags.py
+++ TestApp/get_last_tags.py
@@ -1,32 +1,62 @@
 import os
 import requests
+import json
 
 # GitHub repository information
 owner = "tomasbkk"
 repo = "testrepo"
 
-# GitHub API endpoint to list tags
-api_url = f"https://api.github.com/repos/{owner}/{repo}/tags"
+# GitHub GraphQL API endpoint
+api_url = "https://api.github.com/graphql"
 
-# Make a GET request to the API
-response = requests.get(api_url)
+# GitHub Personal Access Token (replace with your token)
+access_token = "YOUR_ACCESS_TOKEN"
+
+# GraphQL query to fetch the last two tags
+query = """
+{
+  repository(owner: "%s", name: "%s") {
+    refs(refPrefix: "refs/tags/", last: 2) {
+      edges {
+        node {
+          name
+          target {
+            ... on Tag {
+              tagger {
+                date
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+""" % (owner, repo)
+
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+
+# Make a POST request to the GraphQL API
+response = requests.post(api_url, json={"query": query}, headers=headers)
 
 # Initialize variables to store the last two tags
 last_two_tags = []
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Parse the JSON response
-    tags = response.json()
+    data = response.json()
+    tags = data.get("data", {}).get("repository", {}).get("refs", {}).get("edges", [])
 
-    # Extract tag names and create dates
-    tag_dates = [(tag['name'], tag['commit']['author']['date']) for tag in tags]
+    # Extract tag names and tagger dates
+    tag_info = [(tag["node"]["name"], tag["node"]["target"]["tagger"]["date"]) for tag in tags]
 
-    # Sort tags by their creation dates
-    sorted_tags = sorted(tag_dates, key=lambda x: x[1])
+    # Sort tags by their tagger dates
+    sorted_tags = sorted(tag_info, key=lambda x: x[1], reverse=True)
 
     # Get the last 2 tags (the most recent ones)
-    last_two_tags = sorted_tags[-2:]
+    last_two_tags = sorted_tags[:2]
 
     # Print the names of the last two tags
     for tag_name, _ in last_two_tags:
@@ -34,7 +64,7 @@ if response.status_code == 200:
 
 # Set GitHub environment variables for the last two tags
 if last_two_tags:
-    os.environ["LAST_TAG"] = last_two_tags[-1][0]
-    os.environ["SECOND_LAST_TAG"] = last_two_tags[-2][0]
+    os.environ["LAST_TAG"] = last_two_tags[0][0]
+    os.environ["SECOND_LAST_TAG"] = last_two_tags[1][0]
 else:
     print("Failed to fetch tags. Status code:", response.status_code)
