f416702265c37f47bafc8508f5cadd25c8e2268c
TomasBkk <pr0digy1@hotmail.com>
2023-09-12 10:46:31 +0200

    updated

--- TestApp/get_last_tags.py
+++ TestApp/get_last_tags.py
@@ -19,19 +19,23 @@ if response.status_code == 200:
     # Parse the JSON response
     tags = response.json()
 
-    # Sort tags by their creation date (tagger -> date or commit -> committer -> date)
-    sorted_tags = sorted(tags, key=lambda x: x['commit']['committer']['date'], reverse=True)
+    # Extract and convert tag creation dates to datetime objects
+    tag_dates = [(tag['name'], tag['commit']['committer']['date']) for tag in tags]
+
+    # Sort tags by their creation dates in ascending order
+    sorted_tags = sorted(tag_dates, key=lambda x: x[1])
 
     # Get the last 2 tags (the most recent ones)
-    last_two_tags = sorted_tags[:2]
+    last_two_tags = sorted_tags[-2:]
 
     # Print the names of the last two tags
-    for tag in last_two_tags:
-        print("Tag Name:", tag["name"])
+    for tag_name, _ in last_two_tags:
+        print("Tag Name:", tag_name)
 
 # Set GitHub environment variables for the last two tags
 if last_two_tags:
-    os.environ["LAST_TAG"] = last_two_tags[0]["name"]
-    os.environ["SECOND_LAST_TAG"] = last_two_tags[1]["name"]
+    os.environ["LAST_TAG"] = last_two_tags[-1][0]
+    os.environ["SECOND_LAST_TAG"] = last_two_tags[-2][0]
 else:
     print("Failed to fetch tags. Status code:", response.status_code)
+
