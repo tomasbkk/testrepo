ee4b959293935cfbe62472eacc11cfa4cbfd025f
TomasBkk <pr0digy1@hotmail.com>
2023-09-12 10:32:54 +0200

    updated

--- TestApp/get_last_tags.py
+++ TestApp/get_last_tags.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 # GitHub repository information
@@ -10,18 +11,25 @@ api_url = f"https://api.github.com/repos/{owner}/{repo}/tags"
 # Make a GET request to the API
 response = requests.get(api_url)
 
+# Initialize variables to store the last two tags
+last_two_tags = []
+
 # Check if the request was successful
 if response.status_code == 200:
     # Parse the JSON response
     tags = response.json()
 
-    # Get the last 2 tags (which are the most recent ones)
-    last_two_tags = tags[-2:]
+    # Sort tags by their creation date (tagger -> date)
+    sorted_tags = sorted(tags, key=lambda x: x['commit']['tagger']['date'], reverse=True)
 
-    # Print the last 2 tags
-    for tag in last_two_tags:
-        print("Tag Name:", tag["name"])
-        print("Commit SHA:", tag["commit"]["sha"])
-        print("--------")
+    # Get the last 2 tags (the most recent ones)
+    last_two_tags = sorted_tags[:2]
+
+# Set GitHub environment variables for the last two tags
+if last_two_tags:
+    os.environ["LAST_TAG"] = last_two_tags[0]["name"]
+    os.environ["SECOND_LAST_TAG"] = last_two_tags[1]["name"]
+    print("Tag Name 1:", last_two_tags[0]["name"])
+    print("Tag Name 2:", last_two_tags[1]["name"])
 else:
     print("Failed to fetch tags. Status code:", response.status_code)
