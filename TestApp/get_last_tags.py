885e9a5138d2ee0b1d4ea0677b4258f5108d3378
TomasBkk <pr0digy1@hotmail.com>
2023-09-12 10:37:42 +0200

    Improve release notes - #2985 - Added get_git_tags.bat which retrieves the last 2 git tags of the repository and sets them as env vars.

--- TestApp/get_last_tags.py
+++ TestApp/get_last_tags.py
@@ -14,23 +14,35 @@ response = requests.get(api_url)
 # Initialize variables to store the last two tags
 last_two_tags = []
 
+# Function to get the date from a tag object
+def get_tag_date(tag):
+    try:
+        # Try to get the tagger date, and if it's not available, use the committer date
+        return tag['commit']['tagger']['date']
+    except KeyError:
+        try:
+            return tag['commit']['committer']['date']
+        except KeyError:
+            return ""
+
 # Check if the request was successful
 if response.status_code == 200:
     # Parse the JSON response
     tags = response.json()
 
-    # Sort tags by their creation date (tagger -> date or commit -> committer -> date)
-    sorted_tags = sorted(tags, key=lambda x: x['commit'].get('tagger', x['commit']['committer'])['date'], reverse=True)
+    # Sort tags by their creation date (using the get_tag_date function)
+    sorted_tags = sorted(tags, key=get_tag_date, reverse=True)
 
     # Get the last 2 tags (the most recent ones)
     last_two_tags = sorted_tags[:2]
+
+    # Print the names of the last two tags
+    for tag in last_two_tags:
+        print("Tag Name:", tag["name"])
 
 # Set GitHub environment variables for the last two tags
 if last_two_tags:
     os.environ["LAST_TAG"] = last_two_tags[0]["name"]
     os.environ["SECOND_LAST_TAG"] = last_two_tags[1]["name"]
-    print("Tag Name 1:", last_two_tags[0]["name"])
-    print("Tag Name 2:", last_two_tags[1]["name"])
 else:
     print("Failed to fetch tags. Status code:", response.status_code)
-    
